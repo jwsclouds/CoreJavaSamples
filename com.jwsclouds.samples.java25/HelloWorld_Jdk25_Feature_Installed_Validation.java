7c7afc8d922f3f6777ec73a3aa4578bb1ad194c3
jwsclouds-bk <jws.clouds@gmail.com>
2025-11-19 20:04:26 -0800

    removed out folder and update to jdk25

---
+++ com.jwsclouds.samples.java25/HelloWorld_Jdk25_Feature_Installed_Validation.java
@@ -0,0 +1,6 @@
+class HelloWorld_Jdk25_Feature_Installed_Validation {
+    void main() {
+        System.out.println("Hello, World!");
+        System.out.println("Compiler changed to JDK 25");
+    }
+}
